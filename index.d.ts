3a3020e46f4d97e174705204284a8c523b662cfc
cliedeman <ciaranliedeman@gmail.com>
2019-03-28 22:46:13 +0200

    Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare module "pg-range" {
+  import { default as StRange } from "strange";
+
+  // Must use any here as we cannot use a namespace as a type
+  export function install(pg: any): void;
+
+  type Endpoint = Date | number | string;
+
+  export class Range<T extends Endpoint> extends StRange<T> {
+    toPostgres(prepare: any): string;
+  }
+}

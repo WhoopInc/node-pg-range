c8d2618666909659808583286a4c181d8728a78c
Peter Wong <petercpwong@gmail.com>
2022-11-04 23:31:14 +0800

    widen Endpoint type
    Widen `Endpoint` type to accept any primitive or object with a `valueOf` method
    
    See updated upstream package `strange`: https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/62819

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ declare module "pg-range" {
   // Must use any here as we cannot use a namespace as a type
   export function install(pg: any): void;
 
-  type Endpoint = Date | number | string;
+  type Endpoint = Date | number | string | { valueOf(): string | number };
 
   export class Range<T extends Endpoint> extends StRange<T> {
     toPostgres(prepare: any): string;
